217d8d60c71b93467bf854e46bbd124c7d6b6035
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-11 17:26:30 +0200

    removed unused classes

--- src/app/ChoiceBox.tsx
+++ src/app/ChoiceBox.tsx
@@ -72,26 +72,24 @@ const ChoiceBox: React.FC<ChoiceBoxProps> = ({ children, position, onYes, onNo }
   }, []);
 
   return (
-    <a-entity interactive-model className="interactive-model" position={position} scale="2 2 2">
-      <a-light className="interactive-model__light" type="ambient" color="#FFF" intensity={lightIntensity}></a-light>
-      <a-entity ref={modelRef} className="interactive-model__model" onClick={displayInfo}>
+    <a-entity position={position} scale="2 2 2">
+      <a-light type="ambient" color="#FFF" intensity={lightIntensity}></a-light>
+      <a-entity ref={modelRef} onClick={displayInfo}>
         {children}
       </a-entity>
-      <a-box className="interactive-model__panel" height="100" width="100" depth="1"
+      <a-box height="100" width="100" depth="1"
         color="#000" opacity="0.4" position="0 120 -90" scale="1 1 1" rotation="0 90 0" visible={showInfo}
         transparent="true">
-        <a-text className="interactive-model__question" position="-48 19 2.5" color="#FFF"
+        <a-text position="-48 19 2.5" color="#FFF"
           value="Want you to \nburn an incense \nstick?" height="100" width="100" scale="3 3 3"></a-text>
           <HoverBox defaultColor="#000" hoverColor="#F00" onClick={onClickYes} opacity="0.4" position="-31.864 -31.038 2.5" height="30" width="30" depth="1">
             <a-text
-              className="interactive-model__label"
               style={{ pointerEvents: "none" }}
               position="-10.208 -0.010 2.5" color="#FFF" value="Yes" height="100" width="50" text="height: 200"
               scale="5 5 5"></a-text>
           </HoverBox>
         <HoverBox onClick={onClickNo} defaultColor="#000" hoverColor="#F00" opacity="0.4" position="30.502 -31.038 2.5" height="30" width="30" depth="1">
           <a-text
-            className="interactive-model__label"
             style={{ pointerEvents: "none" }}
             position="-7.876 -0.010 2.5" color="#FFF" value="No" height="100" width="50" text="height: 200"
             scale="5 5 5"></a-text>
