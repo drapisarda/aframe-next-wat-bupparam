b3ede7fc273cc4e4cf5aca507b6244d8fd140d6a
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-13 15:38:08 +0200

    IncenseStick style

--- src/app/IncenseStick.tsx
+++ src/app/IncenseStick.tsx
@@ -8,8 +8,12 @@ export type IncenseStickProps = {
 const IncenseStick = ({position, rotation}: IncenseStickProps) => {
   return (
     <a-entity position={position} rotation={rotation}>
-      <a-box height="50" width="1" depth="1" color="#c4a46b"></a-box>
-      <a-box height="20" width="1" depth="1" position="0 -35 0" color="#974038"></a-box>
+      <a-cylinder height="1" radius="0.5" color="#ABABAB" position="0 26 0" normal-texture-offset="0"></a-cylinder>
+      <a-cylinder height="0.3" radius="0.5" color="#F00" emissive="#F00" emissiveIntensity="1" position="0 25 0"></a-cylinder>
+      <a-entity>
+        <a-cylinder height="50" radius="0.5" color="#c4a46b" src="/assets/images/incense_text.min.jpg"></a-cylinder>
+      </a-entity>
+      <a-box height="20" width="0.3" depth="0.3" position="0 -35 0" color="#974038"></a-box>
     </a-entity>
   )
 }
