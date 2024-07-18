0ecfa8d60c6f1b752f0fc823e0c8f9c4fa19c81a
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-18 15:33:13 +0200

    fixing types

--- src/aframe-types.d.ts
+++ src/aframe-types.d.ts
@@ -1,56 +1,38 @@
+declare type common = {
+  position?: string
+  height?: string | number
+  width?: string | number
+  depth?: string | number
+  rotation?: string | number
+  cursor?: string
+  geometry?: string
+  material?: string
+  opacity?: string
+  color?: string
+  scale?: string | number
+  visible?: boolean
+  transparent?: boolean
+  emissive?: string | number
+  emissiveIntensity?: string | number
+  src?: string
+  animation?: string | number
+}
+
 declare namespace JSX {
   interface IntrinsicElements {
     'a-box': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      position?: string
-      rotation?: string
-      scale?: string
-      color?: string
-      opacity?: number | string
-      height?: string | number
-      width?: string | number
-      depth?: string | number
-      visible?: boolean
-      transparent?: boolean
-    }
+      HTMLElement, common>
     'a-cylinder': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      position?: string
-      color?: string
-      opacity?: number | string
-      height?: string | number
-      width?: string | number
-      depth?: string | number
-      emissive?: string | number
-      emissiveIntensity?: string | number
-      src?: string
-      material?: string
-      radius?: string
-    }
+      HTMLElement, common>
     'a-entity': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      position?: string
-      height?: string | number
-      width?: string | number
-      rotation?: string | number
-      scale?: string | number
-      animation?: string | number
-    }
+      HTMLElement, common>
     'a-text': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
+      HTMLElement, common
     > & {
-      position?: string
-      height?: string | number
-      width?: string | number
-      rotation?: string | number
-      scale?: string | number
       value?: string
       text?: string
       align?: string
@@ -59,23 +41,12 @@ declare namespace JSX {
     }
     'a-cursor': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
+      HTMLElement, common
     > & {
       raycaster?: string
-      geometry?: string
-      material?: string
     }
     'a-plane': React.DetailedHTMLProps<
       React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      opacity?: number | string
-      position?: string
-      height?: string | number
-      width?: string | number
-      rotation?: string | number
-      visible?: boolean
-      transparent?: boolean
-    }
+      HTMLElement, common>
   }
 }
